a28d485fec387bc913fd27cb5fef7dcff44663b7
jihyeon-Do <ehwlgus22@gmail.com>
2024-04-04 06:11:04 +0900

    👽️ API : boxoffice api 연결 및 리스트 가공

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,5 @@
 import React, { useEffect, useState, useCallback, useRef } from 'react';
-import {
-  boxofficeList,
-  starRating,
-  individualList,
-} from '../data/CineSuggestionMovieList';
+import { starRating, individualList } from '../data/CineSuggestionMovieList';
 // import { useDispatch, useSelector } from 'react-redux';
 // import { RootState } from '../redux/modules';
 
@@ -15,12 +11,22 @@ import FooterTemplate from '../components/FooterTemplate';
 import Owlslide from '../components/Owlslide';
 import VideoFrameContainer from '../containers/VideoFrameContainer';
 import CineSuggestion from '../components/CineSuggestion';
+import axios from 'axios';
 // import { startGetBoxOfficeListActionCreator } from '../redux/modules/boxoffice';
 // import BoxOffice from '../components/BoxOffice';
 // import TokenService from '../service/TokenService';
 // import AccountService from '../service/AccountService';
 // import CineSuggestion from '../components/CineSuggestion';
 // import AverageRanking from '../components/AverageRanking';
+
+type BoxOfficeList = {
+  movieId?: number;
+  rank?: number;
+  movieTitle?: string;
+  nation?: string;
+  productionYear?: number;
+  runningTime?: number;
+};
 
 function Home() {
   // const dispatch = useDispatch();
@@ -29,6 +35,8 @@ function Home() {
   //   return state.boxoffice.boxOfficeList;
   // });
   const [modalId, setModalId] = useState(0);
+  const [boxofficeList, setBoxofficeList] = useState<BoxOfficeList[]>([]);
+  const LOCALAPI = 'http://3.38.64.130:8080';
   // const token = useSelector((state: RootState) => state.auth.token);
   // const account = useSelector((state) => state.auth.account);
   // const userName = useSelector((state) => state.auth.userName);
@@ -45,6 +53,61 @@ function Home() {
   const hide = () => {
     setModalId(0);
   };
+
+  const getToday = () => {
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = ('0' + (date.getMonth() + 1)).slice(-2);
+    const day = ('0' + date.getDate()).slice(-2);
+    return year + month + day;
+  };
+
+  useEffect(() => {
+    const today = getToday();
+    async function getMovieInfo() {
+      try {
+        const response = await axios.get(
+          `${LOCALAPI}/api/movies/boxoffice?end_date=${'20240328'}`,
+        );
+        let boxofficeArray: BoxOfficeList[] = [];
+        response.data.list.map(
+          (v: { movie_id: number; rank: number }, i: number) => {
+            boxofficeArray = [
+              ...boxofficeArray,
+              { movieId: v.movie_id, rank: v.rank },
+            ];
+          },
+        );
+        boxofficeDetail(boxofficeArray);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getMovieInfo();
+  }, []);
+
+  async function boxofficeDetail(boxofficeArray: BoxOfficeList[]) {
+    let copyBoxofficeList: BoxOfficeList[] = [];
+    await Promise.all(
+      boxofficeArray.map(async (v, i) => {
+        const boxofficeListDetail = await axios.get(
+          `${LOCALAPI}/api/movies/${v.movieId}`,
+        );
+        copyBoxofficeList = [
+          ...copyBoxofficeList,
+          {
+            rank: v.rank,
+            movieId: v.movieId,
+            movieTitle: boxofficeListDetail.data.data.movie_title,
+            nation: boxofficeListDetail.data.data.nation,
+            productionYear: boxofficeListDetail.data.data.production_year,
+            runningTime: boxofficeListDetail.data.data.running_time,
+          },
+        ];
+      }),
+    );
+    setBoxofficeList(copyBoxofficeList);
+  }
 
   // const getBoxOfficeList = useCallback(() => {
   //   dispatch(startGetBoxOfficeListActionCreator());

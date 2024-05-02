625ae58b6bbe51b3dd189f74897a9bc7708cd69e
jihyeon-Do <ehwlgus22@gmail.com>
2024-05-02 22:27:47 +0900

    💄 STYLE : Complete iframe video resposive style

--- src/components/VideoFrame.tsx
+++ src/components/VideoFrame.tsx
@@ -12,22 +12,22 @@ function MainVideoPotal({ hide, id }: VideoFrameContainerProps) {
   const videos = [
     {
       id: 1,
-      title: '기적',
+      title: '이승윤 콘서트 도킹 : 리프트오프',
       src: `https://www.youtube.com/embed/F-QaVTwt4KY?si=smZPSbc3IRabBZD7${autoPalyUrl}`,
     },
     {
       id: 2,
-      title: '듄',
+      title: '[위드키즈]래빗스쿨 2: 부활절 대소동',
       src: `https://www.youtube.com/embed/r4vMqiPPqdQ?si=yyfaiEu0AIKK8wtE${autoPalyUrl}`,
     },
     {
       id: 3,
-      title: '용과 주근깨 공주',
+      title: '[위드키즈]브레드이발소: 셀럽 인 베이커리타운',
       src: `https://www.youtube.com/embed/YCSaw66rkWc?si=phaSKGMhgzUj9SRc${autoPalyUrl}`,
     },
     {
       id: 4,
-      title: '화이트데이',
+      title: '비키퍼',
       src: `https://www.youtube.com/embed/1M9S_awwqw8?si=9xE6O9RXELQ5HmQm"${autoPalyUrl}`,
     },
   ];
@@ -40,11 +40,9 @@ function MainVideoPotal({ hide, id }: VideoFrameContainerProps) {
   }, []);
   return (
     <div className={styles['video-background']} onClick={close}>
-      {
+      <div className="iframe-wrapper">
         <iframe
           title={matchMovie.title}
-          width="920"
-          height="515"
           src={matchMovie.src}
           allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
         >
@@ -53,7 +51,7 @@ function MainVideoPotal({ hide, id }: VideoFrameContainerProps) {
             <div className={styles['btn']}></div>
           </button>
         </iframe>
-      }
+      </div>
     </div>
   );
 }

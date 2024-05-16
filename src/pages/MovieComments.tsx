279240698440a2887d450eec897a5760b009b7ac
jihyeon-Do <ehwlgus22@gmail.com>
2024-05-17 05:49:04 +0900

    🐛 FIX : movie comment not working

--- src/pages/MovieComments.tsx
+++ src/pages/MovieComments.tsx
@@ -90,6 +90,8 @@ function MovieComments() {
           } else {
             setMovieComments(copyMovieComments);
           }
+        } else {
+          setMovieComments(copyMovieComments);
         }
       });
     };

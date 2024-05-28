2ec814d57fd73757bc4f807a1f488cdf612cae7c
jihyeon-Do <ehwlgus22@gmail.com>
2024-05-28 19:35:36 +0900

    🚚 MOVE : Move common styles reset => common

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import './styles/reset.scss';
+import './styles/common.scss';
 
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';

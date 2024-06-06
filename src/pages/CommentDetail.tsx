ea5e925e71cecbe01611a784f61ff9a3b04afbf2
jihyeon-Do <ehwlgus22@gmail.com>
2024-06-07 02:25:02 +0900

    💄 STYLE : Change comment like count state

--- src/pages/CommentDetail.tsx
+++ src/pages/CommentDetail.tsx
@@ -49,7 +49,9 @@ interface Reply {
 
 function CommentDetail() {
   //: movie id는 location state에서 가져오고 comment id는 path에서 가져온다.
-  const [commentContent, setCommentContent] = useState<CommentContent>();
+  const [commentContent, setCommentContent] = useState<CommentContent | null>(
+    null,
+  );
   const [replies, setReplies] = useState<Reply[]>([]);
   const [value, setValue] = useState('');
   const [comment, setComment] = useState<Comment | null>(null);
@@ -83,7 +85,7 @@ function CommentDetail() {
     const year = date.getFullYear();
     const month = date.getMonth();
     const getDate = date.getDate();
-    const createdDate = `${year}-${month < 10 ? '0' + month : month}-${getDate}`;
+    const createdDate = `${year}-${month < 10 ? '0' + month : month}-${getDate < 10 ? '0' + getDate : getDate}`;
     return createdDate;
   };
 
@@ -266,14 +268,30 @@ function CommentDetail() {
           `${LOCALAPI}/api/user-reports/-/movies/${locationState.movieId}/comments/${commentId}/like`,
           bearer_header,
         );
-        if (response.status === 200) setIsLike(false);
+        if (response.status === 204) {
+          if (commentContent) {
+            setCommentContent({
+              ...commentContent,
+              like: commentContent.like - 1,
+            });
+          }
+          setIsLike(false);
+        }
       } else {
         const response = await axios.post(
           `${LOCALAPI}/api/user-reports/-/movies/${locationState.movieId}/comments/${commentId}/like`,
           {},
           bearer_header,
         );
-        if (response.status === 200) setIsLike(true);
+        if (response.status === 200) {
+          if (commentContent) {
+            setCommentContent({
+              ...commentContent,
+              like: commentContent.like + 1,
+            });
+          }
+          setIsLike(true);
+        }
       }
     } catch (error) {
       console.log(error);

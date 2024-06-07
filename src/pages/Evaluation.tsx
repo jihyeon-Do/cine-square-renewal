c9aa0540685b2e4465ac95b1580713f95d144b09
jihyeon-Do <ehwlgus22@gmail.com>
2024-06-07 19:18:59 +0900

    🔥 REMOVE : console log

--- src/pages/Evaluation.tsx
+++ src/pages/Evaluation.tsx
@@ -71,8 +71,6 @@ export default function Evaluation() {
     getRandomMovie();
   }, [page]);
 
-  console.log(page);
-
   useEffect(() => {
     const getEvaluatedMovies = async () => {
       const response = await axios.get(

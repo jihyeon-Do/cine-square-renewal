1d044b58febd926caae9ef19f217ebf0e75560cf
jihyeon-Do <ehwlgus22@gmail.com>
2024-09-17 23:21:05 +0900

    💄 STYLE : Change main slide content and Change slide button position

--- src/components/VideoFrame.tsx
+++ src/components/VideoFrame.tsx
@@ -12,23 +12,23 @@ function MainVideoPotal({ hide, id }: VideoFrameContainerProps) {
   const videos = [
     {
       id: 1,
-      title: '이승윤 콘서트 도킹 : 리프트오프',
-      src: `https://www.youtube.com/embed/F-QaVTwt4KY?si=smZPSbc3IRabBZD7${autoPalyUrl}`,
+      title: '우리가 끝이야',
+      src: `https://www.youtube.com/embed/KuLthg1eCIg?si=M3GuSVOtIbN9iJ_x${autoPalyUrl}`,
     },
     {
       id: 2,
-      title: '[위드키즈]래빗스쿨 2: 부활절 대소동',
-      src: `https://www.youtube.com/embed/r4vMqiPPqdQ?si=yyfaiEu0AIKK8wtE${autoPalyUrl}`,
+      title: '테인티드 러브',
+      src: `https://www.youtube.com/embed/Kzrue9nAVwU?si=Ofi8b6tGAmOZLYaj${autoPalyUrl}`,
     },
     {
       id: 3,
-      title: '[위드키즈]브레드이발소: 셀럽 인 베이커리타운',
-      src: `https://www.youtube.com/embed/YCSaw66rkWc?si=phaSKGMhgzUj9SRc${autoPalyUrl}`,
+      title: '트랜스포머 ONE',
+      src: `https://www.youtube.com/embed/wKiwHP3s5hI?si=1Hvp15i3kP8R_pBn${autoPalyUrl}`,
     },
     {
       id: 4,
-      title: '비키퍼',
-      src: `https://www.youtube.com/embed/1M9S_awwqw8?si=9xE6O9RXELQ5HmQm"${autoPalyUrl}`,
+      title: '오지: 사라진 숲을 찾아서',
+      src: `https://www.youtube.com/embed/--qKYen3N7o?si=K_mkCW0mL6W65jtp${autoPalyUrl}`,
     },
   ];
 
